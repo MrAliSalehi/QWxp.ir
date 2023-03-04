0983354b966bd2720065aff752337acb95086a61
Ali <alisalehi498@yahoo.com>
2023-03-04 21:38:11 +0330

    const file names

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import {build, defineConfig} from 'vite'
+
+export default defineConfig({
+    build: {
+         rollupOptions:{
+             output:{
+                 entryFileNames: `assets/[name].js`,
+                 chunkFileNames: `assets/[name].js`,
+                 assetFileNames: `assets/[name].[ext]`
+             }
+         }
+    }
+})
